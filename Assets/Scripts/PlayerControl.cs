96c4af3e6c6055ec67869949fa08c1627155ad0e
psh25 <psh25@mails.tsinghua.edu.cn>
2026-02-02 11:59:28 +0800

    initial commit: Unity project setup

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float player_x = 0;
+    public float player_y = 0;
+    public float player_z = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player_x = 0;
+        player_y = 0;
+        player_z = 0;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+}
