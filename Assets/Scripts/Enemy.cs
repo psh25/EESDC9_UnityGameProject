c8bd7ddf403862766fb1a61f3369a211e255237d
psh25 <psh25@mails.tsinghua.edu.cn>
2026-02-09 10:10:19 +0800

    add enemy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,8 +1,19 @@
 using UnityEngine;
 
-public class Box : Entity
+public class Enemy : Entity
 {
-    // 被攻击时尝试沿攻击方向移动一格
+    // 死亡处理：删除自身
+    public void Die()
+    {
+        if (GridManager != null)
+        {
+            GridManager.ClearOccupant(GridPosition);
+        }
+
+        Destroy(gameObject);
+    }
+
+    // 被攻击时的响应
     public override void Onhit(Vector2Int attackDirection)
     {
         if (GridManager == null)
@@ -19,12 +30,15 @@ public class Box : Entity
         Entity target = GridManager.GetOccupant(targetPos);
         if (target is Box || target is Player)
         {
+            Die();
             return;
         }
 
         if (target is Enemy enemy)
         {
             enemy.Die();
+            Die();
+            return;
         }
 
         TryMove(attackDirection);
