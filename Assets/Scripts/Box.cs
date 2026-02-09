29327caaca1b6189f43ef772aa2343ef858397bc
psh25 <psh25@mails.tsinghua.edu.cn>
2026-02-09 09:55:41 +0800

    add box

---
+++ Assets/Scripts/Box.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class Box : Entity
+{
+    // 被攻击时尝试沿攻击方向移动一格
+    public override void Onhit(Vector2Int attackDirection)
+    {
+        if (GridManager == null)
+        {
+            return;
+        }
+
+        Vector2Int targetPos = GridPosition + attackDirection;
+        if (!GridManager.IsValidPosition(targetPos))
+        {
+            return;
+        }
+
+        Entity target = GridManager.GetOccupant(targetPos);
+        if (target is Box || target is Player)
+        {
+            return;
+        }
+
+        if (target != null)
+        {
+            target.Onhit(attackDirection);
+        }
+
+        TryMove(attackDirection);
+    }
+}
